d4c84dbad826c946dbd85effb6094128b941a387
Josué Medina Guerrero <josuemedina.032810@gmail.com>
2025-04-25 16:42:50 -0500

    rutas basicas configuradas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+
 import 'package:widgets_app/config/theme/app_theme.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 void main() {
@@ -13,8 +16,12 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: AppTheme().getTheme(),
+      theme: AppTheme(selectedColor: 2).getTheme(),
       home: HomeScreen(),
+      routes: {
+        '/buttons': (context) => ButtonsScreen(),
+        '/cards': (context) => CardsScreen(),
+      },
     );
   }
 }
