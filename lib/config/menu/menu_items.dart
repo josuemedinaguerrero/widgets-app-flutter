9738b5e9f569dc5f2789184e4404812f44773c93
Josué Medina Guerrero <josuemedina.032810@gmail.com>
2025-04-29 08:49:07 -0500

    animated container terminado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -42,4 +42,11 @@ const appMenuItems = <MenuItems>[
     link: '/snackbars',
     icon: Icons.info_outline,
   ),
+
+  MenuItems(
+    title: 'Animated container',
+    subTitle: 'Stateful widget animado',
+    link: '/animated',
+    icon: Icons.check_box_outline_blank_rounded,
+  ),
 ];
--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show Random;
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   static const String name = 'animated_screen';
 
   const AnimatedScreen({super.key});
 
   @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double width = 50;
+  double height = 50;
+  Color color = Colors.indigo;
+  double borderRadius = 10.0;
+
+  void changeShape() {
+    final random = Random();
+    width = random.nextInt(300) + 50;
+    height = random.nextInt(300) + 50;
+    borderRadius = random.nextInt(50) + 5;
+
+    color = Color.fromRGBO(
+      random.nextInt(255),
+      random.nextInt(255),
+      random.nextInt(255),
+      1,
+    );
+
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Animated Container')),
+      body: Center(
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 400),
+          curve: Curves.easeInOutCubic,
+          width: width,
+          height: height,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(borderRadius),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: changeShape,
+        child: Icon(Icons.play_arrow_rounded),
+      ),
+    );
   }
 }
